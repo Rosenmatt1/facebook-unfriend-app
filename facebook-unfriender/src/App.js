26b69b44e9efc64a107cba395d51ea35257cdb3d
Matthew Rosenberg <rosenmatt1@gmail.com>
2021-05-27 00:58:44 -0600

    Added fb SDK and got async fetch working in useEffect mount

--- facebook-unfriender/src/App.js
+++ facebook-unfriender/src/App.js
@@ -3,32 +3,32 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-
   // const [count, setCount] = useState(0);
-
   // Similar to componentDidMount and componentDidUpdate:  
 
-  let url =  "https://graph.facebook.com/oauth/access_token?client_id={your-app-id}&client_secret={your-app-secret}&grant_type=client_credentials"
- 
-  
-  useEffect(() => {    
-     console.log("app")
+  // let url = "https://graph.facebook.com/oauth/access_token?client_id={your-app-id}&client_secret={your-app-secret}&grant_type=client_credentials"
+  let url = 'https://jsonplaceholder.typicode.com/todos/1'
 
-     const response = await fetch(url)
-     const json = await response.json()
-     let addCurrent = json.map(card => {
-       card.current = false
-       return card
-     })
-    //  this.setState({ flashcards: addCurrent })
+  // FB.getLoginStatus(function (response) {
+  //   if (response.status === 'connected') {
+  //     var accessToken = response.authResponse.accessToken;
+  //   }
+  // });
 
-     }, []);
+  useEffect(() => {
 
+    async function fetchAPI() {
+      let response = await fetch(url)
+      response = await response.json()
+      console.log(response)
+    }
 
+    fetchAPI()
+    
+  }, []);
 
   return (
     <div className="App">
-
       Facebook Unfriender
     </div>
   );
