3e99f3be742abdf376c1cfc56e7e89cec73d5199
Matthew Rosenberg <rosenmatt1@gmail.com>
2021-05-26 23:09:57 -0600

    Added the facebook url that gives access token

--- facebook-unfriender/src/App.js
+++ facebook-unfriender/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  // const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:  
+
+  let url =  "https://graph.facebook.com/oauth/access_token?client_id={your-app-id}&client_secret={your-app-secret}&grant_type=client_credentials"
+ 
+  
+  useEffect(() => {    
+     console.log("app")
+
+     const response = await fetch(url)
+     const json = await response.json()
+     let addCurrent = json.map(card => {
+       card.current = false
+       return card
+     })
+    //  this.setState({ flashcards: addCurrent })
+
+     }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      Facebook Unfriender
     </div>
   );
 }
