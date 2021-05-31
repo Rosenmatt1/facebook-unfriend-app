77d49ee70b23617539e6bae7abf2d73bed4227bb
Matthew Rosenberg <rosenmatt1@gmail.com>
2021-05-30 23:24:09 -0600

    Added the app id to the index html

--- facebook-unfriender/src/App.js
+++ facebook-unfriender/src/App.js
@@ -9,14 +9,8 @@ function App() {
   // let url = "https://graph.facebook.com/oauth/access_token?client_id={your-app-id}&client_secret={your-app-secret}&grant_type=client_credentials"
   let url = 'https://jsonplaceholder.typicode.com/todos/1'
 
-  // FB.getLoginStatus(function (response) {
-  //   if (response.status === 'connected') {
-  //     var accessToken = response.authResponse.accessToken;
-  //   }
-  // });
 
   useEffect(() => {
-
     async function fetchAPI() {
       let response = await fetch(url)
       response = await response.json()
@@ -24,7 +18,13 @@ function App() {
     }
 
     fetchAPI()
-    
+
+    FB.getLoginStatus(function (response) {
+      if (response.status === 'connected') {
+        var accessToken = response.authResponse.accessToken;
+      }
+    });
+
   }, []);
 
   return (
