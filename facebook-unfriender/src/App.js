ede1e2608906012bfc8553d1c12fbd146c98ffda
Matthew Rosenberg <rosenmatt1@gmail.com>
2021-06-02 23:39:31 -0600

    Created .env file for fb_secret variable and installed fb

--- facebook-unfriender/src/App.js
+++ facebook-unfriender/src/App.js
@@ -6,9 +6,18 @@ function App() {
   // const [count, setCount] = useState(0);
   // Similar to componentDidMount and componentDidUpdate:  
 
+  const appID = '223211789310003'
+  const clientToken = '9e816bf70b56267e494035c385c74fdc'
+  const { FB_SECRET } = process.env
+
   // let url = "https://graph.facebook.com/oauth/access_token?client_id={your-app-id}&client_secret={your-app-secret}&grant_type=client_credentials"
   let url = 'https://jsonplaceholder.typicode.com/todos/1'
 
+  //opens a dialogue box
+  // FB.ui({
+  //   method: 'share',
+  //   href: 'https://developers.facebook.com/docs/'
+  // }, function(response){});
 
   useEffect(() => {
     async function fetchAPI() {
@@ -20,16 +29,50 @@ function App() {
     fetchAPI()
 
     FB.getLoginStatus(function (response) {
+      console.log("ran loginStatusFunction")
       if (response.status === 'connected') {
         var accessToken = response.authResponse.accessToken;
       }
     });
 
+    FB.login(function (response) {
+      console.log("ran FBLogin")
+      if (response.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function (response) {
+          console.log('Good to see you, ' + response.name + '.');
+        });
+      } else {
+        console.log('User cancelled login or did not fully authorize.');
+      }
+    });
+
+    /* make the API call */
+    FB.api(
+      "?id=https://www.facebook.com",
+      {
+        "fields": "engagement"
+      },
+      function (response) {
+        if (response && !response.error) {
+          /* handle the result */
+          console.log(response)
+        }
+      }
+    );
+
   }, []);
 
   return (
     <div className="App">
+
       Facebook Unfriender
+
+      {/* <fb:login-button
+        scope="public_profile,email"
+        onlogin="checkLoginState();">
+      </fb:login-button> */}
+
     </div>
   );
 }
--- facebook-unfriender/src/index.js
+++ facebook-unfriender/src/index.js
@@ -4,6 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// const bizSdk = require('facebook-nodejs-business-sdk');
+
+// const accessToken = '{access-token}';
+// const accountId = 'act_{{adaccount-id}}';
+
+// const FacebookAdsApi = bizSdk.FacebookAdsApi.init(accessToken);
+// const AdAccount = bizSdk.AdAccount;
+// const Campaign = bizSdk.Campaign;
+
+// const account = new AdAccount(accountId);
+// var campaigns;
+    
+// account.read([AdAccount.Fields.name])
+//   .then((account) =>{
+//     return account.getCampaigns([Campaign.Fields.name], { limit: 10 }) // fields array and params
+//   })
+//   .then((result) =>{
+//     campaigns = result
+//     campaigns.forEach((campaign) =>console.log(campaign.name))  
+//   }).catch(console.error);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
