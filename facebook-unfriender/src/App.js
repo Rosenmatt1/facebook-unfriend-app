ecb2504032359662ece93ba596a411b55d743f06
Matthew Rosenberg <rosenmatt1@gmail.com>
2021-06-05 01:17:14 -0600

    Added login button.  Need to figure out http

--- facebook-unfriender/src/App.js
+++ facebook-unfriender/src/App.js
@@ -71,10 +71,7 @@ function App() {
 
       Facebook Unfriender
 
-      {/* <fb:login-button
-        scope="public_profile,email"
-        onlogin="checkLoginState();">
-      </fb:login-button> */}
+{/* <div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false"></div> */}
 
     </div>
   );
